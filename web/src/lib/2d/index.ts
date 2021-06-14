a74f137fc631ecad8b52d64cdda27ff34a0e4c0d
RP <rys@pobox.com>
2021-06-14 11:25:54 -0700

    Cleanup

--- web/src/lib/2d/index.ts
+++ web/src/lib/2d/index.ts
@@ -1,5 +1,3 @@
-export * from './point'
-export * from './rect'
 export * from './size'
 export * from './gridPosition'
 export * from './gridBounds'
--- web/src/lib/sequences/sequence.ts
+++
@@ -1,5 +0,0 @@
-import fluent, { FluentIterable } from 'fluent-iterable'
-
-export type Sequence<T> = FluentIterable<T>
-
-export const sequence = fluent
