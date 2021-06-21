5f33b83c71af47ea7614fc04772ea7363e29d669
RP <rys@pobox.com>
2021-06-21 12:05:36 -0700

    Fix build

--- web/snowpack.config.js
+++ web/snowpack.config.js
@@ -5,9 +5,13 @@ module.exports = {
   },
   plugins: [
     '@snowpack/plugin-react-refresh',
-    '@snowpack/plugin-typescript',
-    "@snowpack/plugin-optimize"
+    '@snowpack/plugin-typescript'
   ],
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: 'es2018',
+  },
   devOptions: {},
   installOptions: {
     installTypes: true,
